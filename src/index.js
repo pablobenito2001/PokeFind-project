17140680390eae0a2d1ac3b498a9e98ce747f55c
Plagga <benitopab@gmail.com>
2022-08-23 17:04:36 -0300

    search input works, but needs some tweaking

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ wrapper = document.getElementById('wrapper'),
 cardHeader = document.getElementById('cardHeader')
 var init = 0;
 
+Array.prototype.findString = function(string){
+    let final = [];
+    for (let i = 0; i < this.length; i++) {
+        if(this[i].name.indexOf(string) !== -1) {
+            if (this[i].name[0] === string[0]) {
+                final.push(this[i])
+                continue;
+            }
+        }else{
+            continue;
+        }
+    }
+    return final;
+}
+
 function pages(init, final){
     deleteNodes(wrapper)
     getDataPoke(`?offset=${init}&limit=${final}`)
@@ -19,7 +34,6 @@ function pages(init, final){
         }
     })
 }
-
 
 window.onload = function(){
     pages(init, nroPages);
@@ -54,3 +68,24 @@ prev = document.getElementById('prev')
             pages(init, nroPages)
         }
     })
+
+const search = document.getElementById('search');
+
+search.addEventListener('input', function(){
+    if (this.value == '') {
+        pages(0, nroPages)
+    }else{
+        getDataPoke('?offset=0&limit=905')
+        .then(elem => elem.results.findString(search.value))
+        .then(poke => {
+            console.log(poke);
+            deleteNodes(wrapper);
+            for (let i = 0; i < poke.length; i++) {
+                getDataPoke(poke[i].url.substr(34))
+                .then(elem => new Pokemon(elem.name, elem.types.map(elem => elem.type.name), elem.id, elem.abilities, elem.stats, elem.sprites.front_default))
+                .then(poke => wrapper.innerHTML += renderPokeCard(poke.getCardData))
+            }
+        })
+
+    }
+})
